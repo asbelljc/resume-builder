8cc364599378358a8c48440e7e1d81952d6e9305
asbelljc <asbelljc@gmail.com>
2021-12-09 01:09:58 -0500

    Finished draft of preview's EducationItem component.

---
+++ src/components/Preview/EducationItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const EducationItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SchoolAndDateWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+class EducationItem extends Component {
+  render() {
+    const { itemData } = this.props;
+
+    return (
+      <EducationItemWrapper>
+        <SchoolAndDateWrapper>
+          <span>{itemData.school}</span>
+          <span>
+            {itemData.startDate} = {itemData.endDate}
+          </span>
+        </SchoolAndDateWrapper>
+        <span>
+          {itemData.degree}, {itemData.concentration}
+        </span>
+        <span>{itemData.location}</span>
+      </EducationItemWrapper>
+    );
+  }
+}
+
+export default EducationItem;
